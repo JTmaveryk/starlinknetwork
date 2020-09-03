24ee9ec179cb542024afe68a67eb09d115b77846
Jack <50677723+JTmaveryk@users.noreply.github.com>
2020-09-02 22:05:57 -0400

    login testing

---
+++ server.js
@@ -0,0 +1,36 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// Only allow people who know the password set in .env to access the site
+var userName = "username";
+var userPass = "password";
+
+var basicAuth = require('basic-auth');
+app.use(function (request, response, next) {
+  var user = basicAuth(request);
+  if (!user || user.name !== userName || user.pass !== userPass) {
+    response.set('WWW-Authenticate', 'Basic realm="site"');
+    return response.status(401).send();
+  }
+  return next();
+});
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
